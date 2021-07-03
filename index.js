0f12e836772d9c2b39f7889d4fc73b0710d9af6f
kna27 <krisharora27@gmail.com>
2021-07-02 21:38:47 -0400

    Added dotenv for api key

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 
 const express = require('express')
 const app = express()
-const port = 3000
+dotenv = require('dotenv')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+const result = dotenv.config()
+
+if (result.error) {
+  throw result.error
+}
+
+// JSON Request: http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v2/?key=result.parsed.API_KEY&appid=440&steamid=76561198959991541&count=1&format=json
+
+console.log(result.parsed.API_KEY)
 app.listen(process.env.PORT || 5000);
 console.log("Listening at http://127.0.0.1:5000");
