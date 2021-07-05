1efe0ebaa8e075f683eac7cb751fe1791f781378
kna27 <krisharora27@gmail.com>
2021-07-04 22:43:23 -0400

    Separate client-side JS into seperate file

---
+++ public/script.js
@@ -0,0 +1,43 @@
+playerStats = window.playerStats;
+if (!playerStats == 0) {
+    let playtimeChartCanvas = document.getElementById("playtimeChart").getContext("2d");
+    let playtimeChart = new Chart(playtimeChartCanvas, {
+        type: "pie",
+        data: {
+            labels: [
+                "Scout", "Soldier", "Pyro", "Demoman", "Heavy", "Engineer", "Medic", "Sniper", "Spy"
+            ],
+            datasets: [{
+                data: [
+                    playerStats['Scout.accum.iPlayTime'],
+                    playerStats['Soldier.accum.iPlayTime'],
+                    playerStats['Pyro.accum.iPlayTime'],
+                    playerStats['Demoman.accum.iPlayTime'],
+                    playerStats['Heavy.accum.iPlayTime'],
+                    playerStats['Engineer.accum.iPlayTime'],
+                    playerStats['Medic.accum.iPlayTime'],
+                    playerStats['Sniper.accum.iPlayTime'],
+                    playerStats['Spy.accum.iPlayTime']],
+                backgroundColor: [
+                    'rgb(192,192,192)',
+                    'rgb(0,0,255)',
+                    'rgb(255,0,0)',
+                    'rgb(255, 145, 0)',
+                    'rgb(100, 0, 200)',
+                    'rgb(255, 215, 0)',
+                    'rgb(255, 0, 171)',
+                    'rgb(0, 100, 0)',
+                    'rgb(181, 100, 29)'
+                ],
+            }],
+
+        },
+        options: {
+            responsive: false,
+            title: {
+                display: true,
+                text: "Playtime by Class"
+            }
+        }
+    });
+}
