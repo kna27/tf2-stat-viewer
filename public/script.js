11c46a9126c827335e61e6fe87fc2fbe16bde580
kna27 <krisharora27@gmail.com>
2021-07-05 23:01:03 -0400

    Basic functionaility to map playtime chart

--- public/script.js
+++ public/script.js
@@ -1,13 +1,14 @@
 const CLASSES = ["Scout", "Soldier", "Pyro", "Demoman", "Heavy", "Engineer", "Medic", "Sniper", "Spy"];
+const MAPPREFIXES = ["arena_", "cp_", "ctf_", "koth_", "mvm_", "pass_", "pd_", "pl_", "plr_", "rd_", "sd_", "tc_", "tr_"];
 
 window.onload = function () {
     playerStats = window.playerStats;
-
-
-    //--- OVERVIEW STATS ---//
-
-    // Class play-time chart
     if (!playerStats == 0) {
+
+        //--- OVERVIEW STATS ---//
+
+        // Class play-time chart
+
         let playtimeChartCanvas = document.getElementById("playtimeChart").getContext("2d");
         let playtimeChart = new Chart(playtimeChartCanvas, {
             type: "bar",
@@ -69,26 +70,77 @@ window.onload = function () {
                 }
             }
         });
+
+
+        // Collective stats
+        document.getElementById("numericalStat_Kills").innerHTML = playerStats['Scout.accum.iNumberOfKills'] + playerStats['Soldier.accum.iNumberOfKills'] + playerStats['Pyro.accum.iNumberOfKills'] + playerStats['Demoman.accum.iNumberOfKills'] + playerStats['Heavy.accum.iNumberOfKills'] + playerStats['Engineer.accum.iNumberOfKills'] + playerStats['Medic.accum.iNumberOfKills'] + playerStats['Sniper.accum.iNumberOfKills'] + playerStats['Spy.accum.iNumberOfKills'];
+        document.getElementById("numericalStat_KillAssists").innerHTML = playerStats['Scout.accum.iKillAssists'] + playerStats['Soldier.accum.iKillAssists'] + playerStats['Pyro.accum.iKillAssists'] + playerStats['Demoman.accum.iKillAssists'] + playerStats['Heavy.accum.iKillAssists'] + playerStats['Engineer.accum.iKillAssists'] + playerStats['Medic.accum.iKillAssists'] + playerStats['Sniper.accum.iKillAssists'] + playerStats['Spy.accum.iKillAssists'];
+        document.getElementById("numericalStat_DamageDealt").innerHTML = playerStats['Scout.accum.iDamageDealt'] + playerStats['Soldier.accum.iDamageDealt'] + playerStats['Pyro.accum.iDamageDealt'] + playerStats['Demoman.accum.iDamageDealt'] + playerStats['Heavy.accum.iDamageDealt'] + playerStats['Engineer.accum.iDamageDealt'] + playerStats['Medic.accum.iDamageDealt'] + playerStats['Sniper.accum.iDamageDealt'] + playerStats['Spy.accum.iDamageDealt'];
+        document.getElementById("numericalStat_Dominations").innerHTML = playerStats['Scout.accum.iDominations'] + playerStats['Soldier.accum.iDominations'] + playerStats['Pyro.accum.iDominations'] + playerStats['Demoman.accum.iDominations'] + playerStats['Heavy.accum.iDominations'] + playerStats['Engineer.accum.iDominations'] + playerStats['Medic.accum.iDominations'] + playerStats['Sniper.accum.iDominations'] + playerStats['Spy.accum.iDominations'];
+        document.getElementById("numericalStat_Revenges").innerHTML = playerStats['Scout.accum.iRevenge'] + playerStats['Soldier.accum.iRevenge'] + playerStats['Pyro.accum.iRevenge'] + playerStats['Demoman.accum.iRevenge'] + playerStats['Heavy.accum.iRevenge'] + playerStats['Engineer.accum.iRevenge'] + playerStats['Medic.accum.iRevenge'] + playerStats['Sniper.accum.iRevenge'] + playerStats['Spy.accum.iRevenge'];
+        document.getElementById("numericalStat_PointsScored").innerHTML = playerStats['Scout.accum.iPointsScored'] + playerStats['Soldier.accum.iPointsScored'] + playerStats['Pyro.accum.iPointsScored'] + playerStats['Demoman.accum.iPointsScored'] + playerStats['Heavy.accum.iPointsScored'] + playerStats['Engineer.accum.iPointsScored'] + playerStats['Medic.accum.iPointsScored'] + playerStats['Sniper.accum.iPointsScored'] + playerStats['Spy.accum.iPointsScored'];
+
+
+        //--- CLASS STATS ---//
+
+        showNewClassStats("Scout");
+        CLASSES.forEach((element) => document.getElementById(`class_stat_${element}`).addEventListener("click", function () { showNewClassStats(element) }));
+
+
+        //--- MAP STATS ---//
+
+        mapPlayTimeStats = {}
+        vals = []
+        for (var key in playerStats) {
+            if (key.endsWith(".accum.iPlayTime") && !CLASSES.some(substring => key.includes(substring)) && !key.endsWith(".mvm.accum.iPlayTime")) {
+                mapPlayTimeStats[key] = playerStats[key];
+            }
+        }
     }
 
-    // Collective stats
-    document.getElementById("numericalStat_Kills").innerHTML = playerStats['Scout.accum.iNumberOfKills'] + playerStats['Soldier.accum.iNumberOfKills'] + playerStats['Pyro.accum.iNumberOfKills'] + playerStats['Demoman.accum.iNumberOfKills'] + playerStats['Heavy.accum.iNumberOfKills'] + playerStats['Engineer.accum.iNumberOfKills'] + playerStats['Medic.accum.iNumberOfKills'] + playerStats['Sniper.accum.iNumberOfKills'] + playerStats['Spy.accum.iNumberOfKills'];
-    document.getElementById("numericalStat_KillAssists").innerHTML = playerStats['Scout.accum.iKillAssists'] + playerStats['Soldier.accum.iKillAssists'] + playerStats['Pyro.accum.iKillAssists'] + playerStats['Demoman.accum.iKillAssists'] + playerStats['Heavy.accum.iKillAssists'] + playerStats['Engineer.accum.iKillAssists'] + playerStats['Medic.accum.iKillAssists'] + playerStats['Sniper.accum.iKillAssists'] + playerStats['Spy.accum.iKillAssists'];
-    document.getElementById("numericalStat_DamageDealt").innerHTML = playerStats['Scout.accum.iDamageDealt'] + playerStats['Soldier.accum.iDamageDealt'] + playerStats['Pyro.accum.iDamageDealt'] + playerStats['Demoman.accum.iDamageDealt'] + playerStats['Heavy.accum.iDamageDealt'] + playerStats['Engineer.accum.iDamageDealt'] + playerStats['Medic.accum.iDamageDealt'] + playerStats['Sniper.accum.iDamageDealt'] + playerStats['Spy.accum.iDamageDealt'];
-    document.getElementById("numericalStat_Dominations").innerHTML = playerStats['Scout.accum.iDominations'] + playerStats['Soldier.accum.iDominations'] + playerStats['Pyro.accum.iDominations'] + playerStats['Demoman.accum.iDominations'] + playerStats['Heavy.accum.iDominations'] + playerStats['Engineer.accum.iDominations'] + playerStats['Medic.accum.iDominations'] + playerStats['Sniper.accum.iDominations'] + playerStats['Spy.accum.iDominations'];
-    document.getElementById("numericalStat_Revenges").innerHTML = playerStats['Scout.accum.iRevenge'] + playerStats['Soldier.accum.iRevenge'] + playerStats['Pyro.accum.iRevenge'] + playerStats['Demoman.accum.iRevenge'] + playerStats['Heavy.accum.iRevenge'] + playerStats['Engineer.accum.iRevenge'] + playerStats['Medic.accum.iRevenge'] + playerStats['Sniper.accum.iRevenge'] + playerStats['Spy.accum.iRevenge'];
-    document.getElementById("numericalStat_PointsScored").innerHTML = playerStats['Scout.accum.iPointsScored'] + playerStats['Soldier.accum.iPointsScored'] + playerStats['Pyro.accum.iPointsScored'] + playerStats['Demoman.accum.iPointsScored'] + playerStats['Heavy.accum.iPointsScored'] + playerStats['Engineer.accum.iPointsScored'] + playerStats['Medic.accum.iPointsScored'] + playerStats['Sniper.accum.iPointsScored'] + playerStats['Spy.accum.iPointsScored'];
+    Object.entries(mapPlayTimeStats).forEach(([key, value]) => {
+        vals.push((value / 3600).toFixed(2));
+    });
 
-
-    //--- CLASS STATS ---//
-  
-    showNewClassStats("Scout");
-    CLASSES.forEach((element) => document.getElementById(`class_stat_${element}`).addEventListener("click", function () { showNewClassStats(element) }));
-
-
-    //--- MAP STATS ---//
-
-    // TODO
+    let mapChartCanvas = document.getElementById("mapChart").getContext("2d");
+    let mapChart = new Chart(mapChartCanvas, {
+        type: "bar",
+        data: {
+            labels: Object.keys(mapPlayTimeStats),
+            datasets: [{
+                data: vals,
+                backgroundColor: 'rgb(184, 56, 59)'
+            }],
+            options: {
+                responsive: false,
+                title: {
+                    display: true,
+                    text: "Map Playtime"
+                },
+                plugins: {
+                    legend: {
+                        display: false
+                    }
+                },
+                scales: {
+                    x: {
+                        ticks: {
+                            font: {
+                                size: 15
+                            }
+                        }
+                    },
+                    y: {
+                        ticks: {
+                            font: {
+                                size: 24
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    });
 
 
     //--- MVM STATS ---//
